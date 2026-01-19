a95a89d9b233a19215d33c7a7ac1a320d18e60cb
Darío Rivera <fermius.us@gmail.com>
2026-01-19 17:58:52 -0500

    feat: create vite + vue app

---
+++ app_two/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Substraction from './components/Substraction.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <Substraction msg="Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+</style>
---
+++ app_two/src/components/Substraction.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const a = ref(5)
+const b = ref(2)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <div class="card">
+      <input type="number" id="a" v-model="a" />
+      <span class="operator">-</span>
+      <input type="number" id="b" v-model="b" />
+      <span class="operator">=</span>
+      <input id="result" :value="a - b" readonly=""/>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+  .card {
+    padding: 0.5em;
+    display: flex;
+    align-items: center;
+  }
+
+  .card input {
+    padding: 5px;
+    width: 60px;
+  }
+
+  .operator {
+    color: teal;
+    display: inline-block;
+    padding: 5px;
+    font-size: 20px;
+  }
+</style>
