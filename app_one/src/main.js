f5520523e0cf703d187147a6576b2f99ec6c0640
Darío Rivera <fermius.us@gmail.com>
2026-01-19 17:45:05 -0500

    feat: create vite vanilla JS app

---
+++ app_one/src/main.js
@@ -0,0 +1,40 @@
+import './style.css'
+import javascriptLogo from '/javascript.svg'
+import sum from './sum'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Vanilla!</h1>
+    <div class="card">
+      <label for="x"></label>
+      <input type="number" id="x" value="2" />
+      <span class="operator">+</span>
+      <label for="y"></label>
+      <input type="number" id="y" value="3" />
+      <span class="operator">=</span>
+      <input id="result" readonly />
+    </div>
+  </div>
+`
+
+function setupSum() {
+  const x = document.querySelector('#x')
+  const y = document.querySelector('#y')
+  const result = document.querySelector('#result')
+
+  const listener = () => {
+    result.value = sum(Number(x.value), Number(y.value))
+  }
+
+  listener(); // first time calculation
+
+  x.addEventListener('keyup', listener)
+  y.addEventListener('keyup', listener)
+}
+
+window.onload = () => {
+  setupSum()
+}
---
+++ app_one/src/sum.js
@@ -0,0 +1,11 @@
+/**
+ * Adds two numbers together
+ * @param {number} a - First number
+ * @param {number} b - Second number
+ * @returns {number} The sum of a and b
+ */
+const sum  = (a, b) => {
+  return a + b;
+}
+
+export default sum
