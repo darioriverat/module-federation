9634fd8b0137c2ffa9a19bdc8e06d8ed3f4987da
Darío Rivera <fermius.us@gmail.com>
2026-01-20 00:52:57 -0500

    ci: dockerize applications

--- app_one/vite.config.js
+++ app_one/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [],
   server: {
-    port: 5182,
+    port: 5181,
     host: '0.0.0.0'
   },
 })
