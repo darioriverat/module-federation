88c99544172d79cba2c64bd02cb8b2be09cc787a
Darío Rivera <fermius.us@gmail.com>
2026-01-24 14:11:17 -0500

    feat: integrate app_two as a federated module

--- app_two/vite.config.js
+++ app_two/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    federation({
+      name: 'federated_two',
+      filename: 'two.js',
+      exposes: {
+        './main.js': path.resolve(__dirname, 'src/main.js')
+      }
+    })
+  ],
   server: {
     port: 5182,
     host: '0.0.0.0'
--- host_app/src/main.jsx
+++ host_app/src/main.jsx
@@ -5,7 +5,11 @@ import App from './App.jsx'
 
 const env = import.meta.env.MODE;
 const host_one = import.meta.env.VITE_APP_ONE_HOST;
+const host_two = import.meta.env.VITE_APP_TWO_HOST;
 
+/**
+ * Remote number one
+ */
 const remote_one_location = (env == 'development')
   ? `${host_one}/src/main.js`
   : 'remote_one/one';
@@ -14,6 +18,18 @@ const remote_one_loader = import(remote_one_location)
 const remote_one_module = await remote_one_loader
 
 remote_one_module.mountComponent({elementId: '#app_one'})
+
+/**
+ * Remote number two
+ */
+const remote_two_location = (env == 'development')
+  ? `${host_two}/src/main.js`
+  : 'remote_two/two';
+
+const remote_two_loader = import(remote_two_location)
+const remote_two_module = await remote_two_loader
+
+remote_two_module.mountComponent({elementId: '#app_two'})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
