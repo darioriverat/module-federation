f7c51e9e8c5d57b924fac9c1063be9b4d6838cd6
Darío Rivera <fermius.us@gmail.com>
2026-01-25 20:21:53 -0500

    fix: public base path for images

--- app_one/vite.config.js
+++ app_one/vite.config.js
@@ -1,21 +1,27 @@
 import federation from '@originjs/vite-plugin-federation'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import path from 'node:path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    federation({
-      name: 'federated_one',
-      filename: 'one.js',
-      // exposed modules
-      exposes: {
-        './main': path.resolve(__dirname, 'src/main.js'),
-      }
-    })
-  ],
-  server: {
-    port: 5181,
-    host: '0.0.0.0'
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  const basePath = env.VITE_PUBLIC_BASE_PATH || '/'
+
+  return {
+    base: basePath,
+    plugins: [
+      federation({
+        name: 'federated_one',
+        filename: 'one.js',
+        // exposed modules
+        exposes: {
+          './main': path.resolve(__dirname, 'src/main.js'),
+        }
+      })
+    ],
+    server: {
+      port: 5181,
+      host: '0.0.0.0'
+    },
+  }
 })
--- app_two/vite.config.js
+++ app_two/vite.config.js
@@ -1,22 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import federation from '@originjs/vite-plugin-federation'
 import path from 'node:path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    federation({
-      name: 'federated_two',
-      filename: 'two.js',
-      exposes: {
-        './main': path.resolve(__dirname, 'src/main.js')
-      }
-    })
-  ],
-  server: {
-    port: 5182,
-    host: '0.0.0.0'
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  const basePath = env.VITE_PUBLIC_BASE_PATH || '/'
+
+  return {
+    base: basePath,
+    plugins: [
+      vue(),
+      federation({
+        name: 'federated_two',
+        filename: 'two.js',
+        exposes: {
+          './main': path.resolve(__dirname, 'src/main.js')
+        }
+      })
+    ],
+    server: {
+      port: 5182,
+      host: '0.0.0.0'
+    },
+  }
 })
